2bd45df48bd62fff243fc4060829512d7b9076e8
samhk90 <mdsami9022@gmail.com>
2024-06-01 14:18:12 +0530

    changed skill card

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -1,14 +1,27 @@
 import React from 'react';
-
-const SkillCard = ({ skill, level }) => {
+const images = require.context('../img', true, /\.svg$/);
+const SkillCard = () => {
   return (
-    <div className="skill-card">
-      <h3 className="skill-name">{skill}</h3>
-      <div className="skill-level-container">
-        <div className="skill-level" style={{ width: `${level}%` }}></div>
+    <div className="container-fluid d-flex justify-content-center">
+    <div className="skills-container mx-5 p-3">
+            <h1>Technologies I like the most</h1>
+            <div className="skill-card">
+      <img src={images('./javascript.svg')} alt="" />
+      <img src={images('./html.svg')} alt="" />
+      <img src={images('./css.svg')} alt="" />
+      <img src={images('./python.svg')} alt="" />
+      <img src={images('./django.svg')} alt="" />
+      <img src={images('./react.svg')} alt="" />
+      <img src={images('./mysql.svg')} alt="" />
+      <img src={images('./pg.svg')} alt="" />
+      <img src={images('./mongodb.svg')} alt="" />
+      <img src={images('./git.svg')} alt="" />
+      <img src={images('./docker.svg')} alt="" />
+      
       </div>
-      <p className="skill-level-text">{level}%</p>
+            </div>
     </div>
+
   );
 };
 
